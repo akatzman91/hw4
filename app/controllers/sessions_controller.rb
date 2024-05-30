e39b0514d95b72b453f74f4fc0412e52026fa266
Aaron Katzman <aaron.katzman@kellogg.northwestern.edu>
2024-05-30 15:16:47 +0000

    Updated sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome, #{@user["first_name"]}."
+        redirect_to "/places"
+      else
+        flash["notice"] = "Please try again."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Please try again."
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    flash["notice"] = "You are now logged out."
+    session["user_id"] = nil
+    redirect_to "/login"
   end
 end
   
